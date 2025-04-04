178b127e4d7cdb126326dc59c199dc4362ff2531
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2025-04-04 14:38:26 +0200

    ✨ Use 2 circles

--- app/routes/api.tsx
+++ app/routes/api.tsx
@@ -1,30 +1,51 @@
 import { flushSync } from "react-dom";
+import { type ComponentProps } from "react";
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
 import { Checkbox } from "~/ui/Checkbox";
 import { Circle } from "~/ui/Circle";
 import type { Route } from "./+types/api";
+import { twMerge } from "tailwind-merge";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Live | View transition API" }];
 }
+const SMALL_CIRCLE_ID = "small-circle-id";
+const BIG_CIRCLE_ID = "big-circle-id";
+const SmallCircle: React.FC<ComponentProps<typeof Circle>> = (props) => (
+  <Circle {...props} id={SMALL_CIRCLE_ID} />
+);
+const BigCircle: React.FC<ComponentProps<typeof Circle>> = (props) => (
+  <Circle
+    {...props}
+    id={BIG_CIRCLE_ID}
+    className={twMerge("bg-brand-primary size-40", props.className)}
+  />
+);
 
-const scope = { Checkbox, Circle, flushSync };
+const scope = {
+  Checkbox,
+  SmallCircle,
+  BigCircle,
+  BIG_CIRCLE_ID,
+  SMALL_CIRCLE_ID,
+  flushSync,
+};
 
 const code = `
 function demo() {
-  const ELEMENT_ID = 'circle-id';
-
   const handleInputChange = () => {
-    const element = document.getElementById(ELEMENT_ID);
-    element?.classList.toggle("left-[200px]");
-    element?.classList.toggle("size-40");
+    const smallCircle = document.getElementById(SMALL_CIRCLE_ID);
+    const bigCircle = document.getElementById(BIG_CIRCLE_ID);
+    smallCircle?.classList.toggle("hidden");
+    bigCircle?.classList.toggle("hidden");
   };
 
   return (
     <div className="space-y-4">
       <Checkbox label="Animate" onChange={handleInputChange} />
       <div className="relative min-h-20">
-        <Circle id={ELEMENT_ID} />
+        <SmallCircle  />
+        <BigCircle className="hidden left-[200px] " />
       </div>
     </div>
   );
@@ -33,11 +54,11 @@ function demo() {
 
 export default function Live() {
   return (
-    <main className="size- mx-auto max-w-screen-lg p-4">
+    <main className="mx-auto max-w-screen-xl p-4">
       <h1 className="title-1 w-fit" style={{ viewTransitionName: "title-api" }}>
         API demo
       </h1>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-12">
         <LiveProvider code={code} scope={scope} enableTypeScript language="tsx">
           <LiveEditor />
           <LiveError />
